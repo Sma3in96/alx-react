dcfa5330213959d0fa9aebfdad3871b463cf359a
Sma3in96 <oumouss.ismail5@gmail.com>
2024-06-26 14:24:22 +0100

    testing

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+$(document).ready(function() {
+  $('body').append($('p').text('Holberton Dashboard'));
+  $('body').append($('p').text('Dashboard data for the students'));
+  $('body').append($('p').text('Copyright - Holberton School'));
+});
