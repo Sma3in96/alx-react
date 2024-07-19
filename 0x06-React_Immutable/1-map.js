6d3205fd1d86fdd3af6ccdaecab568e4fae9fbb5
Sma3in96 <oumouss.ismail@gmail.com>
2024-07-19 10:36:12 +0100

    task2

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,4 +1,5 @@
 import { Map } from 'immutable';
-export default function getImmutableObject (obj) {
-  return Map(obj);
+
+export default function getImmutableObject(object) {
+  return Map(object);
 }
